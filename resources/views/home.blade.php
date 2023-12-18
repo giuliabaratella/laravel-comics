c88aa3fa30726c329282ea3dc6f772251369d552
giuliabaratella <giuliabaratella7@gmail.com>
2023-12-18 15:46:29 +0100

    added db file + main section content

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,27 @@
 @extends('layouts.app')
 
-@section('title', 'Home')
+@section('title', 'Comics')
 
 @section('content')
-<main>
-    <h1>Homepage</h1>
-</main>
+    <main>
+
+        <section id="current-series" class="container">
+            <h1>Comics</h1>
+            <div class="row">
+                @foreach ($comics as $comic)
+                    <div class="col-12 col-md-4 col-lg-2">
+                        <div class="card">
+                            <div class="card-img-box">
+                                <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $comic['title'] }}</h5>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </section>
+    </main>
 
 @endsection
