b6942305d8a0833c3507898d88a84f92c7ba040d
giuliabaratella <giuliabaratella7@gmail.com>
2023-12-19 17:27:03 +0100

    updated show page style

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,27 +1,53 @@
 @extends('layouts.app')
 
-@section('title', 'Comics')
+@section('title', $comic['title'])
 
 @section('content')
     <main>
         <div id="jumbo">
         </div>
-        <div class="container py-5">
-            <div class="label text-uppercase fw-bold">Comic Detail</div>
-            <section id="current-series" class="container">
-                <div class="row row-gap-3">
-                    <div class="col-12">
-                        <div class="card-comics">
-                            <div class="img-box mb-3">
-                                <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
-                            </div>
-                            <div class="card-body">
-                                <h6 class="text-uppercase mb-5">{{ $comic['title'] }}</h6>
-                            </div>
-                        </div>
+        <div id="comic-detail">
+            <div class="blueline">
+                <div class="container">
+                    <div class="comic-img">
+                        <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
                     </div>
                 </div>
-            </section>
+            </div>
+            <div class="container py-5">
+
+
+                <section>
+                    <div class="row mt-5">
+                        <div class="col-12 col-lg-8 mb-3">
+                            <h1 class="text-uppercase mb-3">{{ $comic['title'] }}</h1>
+                            <div class="price-info row align-items-center mb-3">
+                                <div class="price-box col-8 d-flex justify-content-between align-items-center">
+                                    <div class="d-flex align-items-center">
+                                        <div class="text-green me-1">U:S:Price:</div>
+                                        <div>{{ $comic['price'] }}</div>
+                                    </div>
+                                    <div class="text-uppercase text-green">Available</div>
+                                </div>
+                                <div class="price-box col-4 d-flex justify-content-center align-items-center">
+                                    <div class="me-3">Check Availability </div>
+                                    <i class="fa-solid fa-angle-down"></i>
+                                </div>
+
+                            </div>
+                            <p>{{ $comic['description'] }}</p>
+                        </div>
+
+                        <div class="col-12 col-lg-4">
+                            <div class="mb-3">
+                                <h6 class="text-uppercase text-end">Advertisement</h6>
+                                <img src="{{ Vite::asset('resources/img/adv.jpg') }}" alt="advertisement">
+                            </div>
+                        </div>
+
+                    </div>
+                </section>
+            </div>
         </div>
 
     </main>
